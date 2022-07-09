3aead75312801161eb72da9f9233b860360eba95
CesurPolat <cesurpolat12@gmail.com>
2022-07-10 02:53:44 +0300

    Api Games

---
+++ Api/app.py
@@ -0,0 +1,16 @@
+from codecs import utf_8_encode
+import json
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/api/Test")
+def hello():
+    f=open("games.json","r")
+    output=[]
+    for x in f.readlines():
+        output.append(x.encode().decode('utf-8'))
+    return {"data":output}
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
--- copyforce.py
+++ copyforce.py
@@ -1,20 +1,5 @@
-from genericpath import exists
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
-class NeuralHTTP(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type","text/html")
-        self.end_headers()
-
-        f = open("Website/404.html","r")
-        if self.path == "/":
-            self.path="/index.html"
-        if exists("Website"+self.path):
-            f = open("Website"+self.path, "r")
-        
-        self.wfile.write(bytes(f.read(),"utf-8"))
-
-server = HTTPServer(("",80),NeuralHTTP)
-print("Server Running...")
-server.serve_forever()
+#Running Website
+os.chdir("Website")
+os.system("py -m http.server")
