c0d34eb0f1de24d7869672077ebc90a493d1a760
CesurPolat <cesurpolat12@gmail.com>
2022-07-11 21:15:59 +0300

    Screen Capture

--- Socket/socket.py
+++ Socket/socket.py
@@ -1,21 +1,32 @@
 import asyncio
-from flask import jsonify
 from websockets import serve
 import json
 import os
+import threading
+import pyautogui
+from io import BytesIO
 
 async def echo(websocket):
-    #await websocket.send("Welcome Message")
     async for message in websocket:
-        await websocket.send(message)
         f=open("games.json","r")
         for appName in f.readlines():
             if json.loads(appName)["name"]==message:
                 os.system("start "+json.loads(appName)["path"])
-            
-
+        x = threading.Thread(target=getScreen,args=(websocket,))
+        x.start()
+ 
+def getScreen(websocket):
+    while True:
+        myScreenshot = pyautogui.screenshot()
+        resizedscreen=myScreenshot.resize((256,144))
+        im_file = BytesIO()
+        resizedscreen.save(im_file, format="JPEG")
+        im_bytes = im_file.getvalue()
+        asyncio.run(asyncio.sleep(0.5))
+        asyncio.run(websocket.send(im_bytes))
+    
 async def main():
-    async with serve(echo, "localhost", 8765):
+    async with serve(echo, "0.0.0.0", 3000):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
